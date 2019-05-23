419fc4891f3bf471e48b0757ea664b4908d600f2
luozhe73 <1043269439@qq.com>
2019-05-23 20:22:16 +0800

    精简代码

--- src/main/resources/static/js/weather/report.js
+++
@@ -1,8 +0,0 @@
-
-$(function () {
-    $("#selectId").change(function () {
-        var cityId = $("#selectId").val();
-        var url='/report/cityId/'+cityId;
-        window.location.href=url;
-    })
-});
